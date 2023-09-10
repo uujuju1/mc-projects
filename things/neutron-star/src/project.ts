1175468e377ea3638b522c59a97c9b674cf70c83
uujuju <84468589+uujuju1@users.noreply.github.com>
2023-09-09 21:33:24 -0300

    Update project.ts

--- things/neutron-star/src/project.ts
+++ things/neutron-star/src/project.ts
@@ -2,9 +2,7 @@ import {makeProject} from '@motion-canvas/core';
 
 import intro from './scenes/intro?scene';
 import example from './scenes/example?scene';
-import audio from '../audio/voice.mp3';
 
 export default makeProject({
   scenes: [intro, example],
-  audio: audio,
 });
